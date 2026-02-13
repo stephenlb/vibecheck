366d97282ed8c5c957a0063c2d69b48ce07b67df
Stephen Blum <blum.stephen@gmail.com>
2026-02-13 11:37:28 -0800

    initial files.

--- README.md
+++ README.md
@@ -1,5 +1,28 @@
 # VibeCheck
 
+No AI training - Classify any text in +100 Languages.
+Dynamic Classifiaction of text in +100 langauges.
+Detect positivity, negativity, priase, nsfw.
+
+## Installation and Running
+
+```shell
+## Login to HG beacuse of license
+hg auth login
+## UV
+uv install -r requirements.txt 
+## OR PIP
+pip install -r requirements.txt
+docker build . -t vibecheck
+```
+
+## TODO
+
+ - ✅ simplify ( and it works )
+ - ✅ Add FastAPI ( turn this into an API )
+ - Dockerizer
+ - Yaml definitions
+
 ## Architecture
 
 'happy joy awesome' => embeddinggemma => baseline vectors (use for distance comparison)
--- main.py
+++ main.py
@@ -1,18 +1,32 @@
-import torch
+import json
+from fastapi import FastAPI, Request
 from sentence_transformers import SentenceTransformer
 
+app = FastAPI()
+
+@app.post("/")
+async def index(request: Request):
+    body = await request.body()
+    text = body.decode("utf-8")
+    #return text
+    return vibe("good, good job!")
+    #return vibe(text)
+
 model = SentenceTransformer("google/embeddinggemma-300m")
-baselineInput = ["happy awesome joy good excellent sweet honey"]
-positivity = torch.tensor(model.encode(baselineInput))
+## TODO in yaml format
+praises = model.encode(["good job, amazing"])
+positivity = model.encode(["happy awesome joy good excellent nice amazing"])
 
 def vibe(sentance: str) -> float:
-    embedding = torch.tensor(model.encode(sentance))
-    cosineSim = torch.nn.functional.cosine_similarity(embedding, positivity).item()
-    #torch.tensor(
-    similar = model.similarity(embedding, [positivity])
-    return similar.detach().cpu().numpy()[0][0]
+    embedding = model.encode([sentance])
+    similar = model.similarity(embedding, positivity)
+    return float(similar.detach().cpu().numpy()[0][0])
 
+## TODO REMOVE this is just for debugging...
 print("bad", vibe("bad, you did badly"))
+print("bad in Lithuanian", vibe("blogai, blogai padarei"))
 print("good", vibe("good, good job!"))
+print("well", vibe("you did well"))
+print("poor", vibe("you did poorly"))
 print("emoji 🎉", vibe("🎉"))
 print("emoji 😞", vibe("😞"))
